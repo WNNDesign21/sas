8abb9c7276f68fde83dab317d54ce7032d667a4f
Lenovo Loq <adhi.fajar.fict@krw.horizon.ac.id>
2025-02-19 22:39:22 +0700

    Ubah Login + CSS

--- application/views/login.php
+++ application/views/login.php
@@ -21,31 +21,37 @@
     <!-- <link href="assets/css/sb-admin-2.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/sb-admin-2.css'); ?>">
 
+    <!-- INI CSS LOCAL -->
+    <link rel="stylesheet" href="assets/css/sas.css">
+
 </head>
 
-<body class="bg-white">
+<body>
+    <div class="background"></div>
+    <div class="element top-left"></div>
+    <div class="element bottom-right"></div>
+    <div class="logo"></div>
 
     <div class="container">
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-12 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-7 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-5">
+                        <div>
+                            <div class="login-container">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Sign In</h1>
                                     </div>
                                     <form class="user" method="post" action="<?= base_url('auth/proses_login'); ?>">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" name="id_user"
-                                                placeholder="Masukan ID User" required>
+                                                placeholder="Masukan NPM" required>
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
@@ -63,7 +69,7 @@
                                                 <?php echo $this->session->flashdata('error'); ?>
                                             </div>
                                         <?php endif; ?>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        <button type="submit" class="btn btn-sas btn-user btn-block">
                                             Login
                                         </button>
 
@@ -71,19 +77,10 @@
                                             Login
                                         </a> -->
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
