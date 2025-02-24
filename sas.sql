6ac66cd68ef2675d516ba9a771c373393cc1a4e9
unknown <wendi.nurrahmansyah.fict@krw.horizon.ac.id>
2025-02-24 19:20:26 +0700

    db terbaru

--- sas.sql
+++ sas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2025 at 02:37 PM
+-- Generation Time: Feb 24, 2025 at 01:19 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.31
 
@@ -29,9 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `absensi` (
   `id_absensi` int(10) UNSIGNED NOT NULL,
-  `id_jadwal` int(11) NOT NULL,
-  `nim` bigint(20) NOT NULL,
+  `id_jadwal` int(10) UNSIGNED NOT NULL,
+  `npm` bigint(20) NOT NULL,
   `tanggal` date NOT NULL,
+  `pertemuan` int(2) NOT NULL,
   `status` enum('HADIR','TIDAK HADIR','IZIN') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,8 +40,11 @@ CREATE TABLE `absensi` (
 -- Dumping data for table `absensi`
 --
 
-INSERT INTO `absensi` (`id_absensi`, `id_jadwal`, `nim`, `tanggal`, `status`) VALUES
-(1, 1, 4337855201230085, '2025-02-15', 'HADIR');
+INSERT INTO `absensi` (`id_absensi`, `id_jadwal`, `npm`, `tanggal`, `pertemuan`, `status`) VALUES
+(1, 1, 4337855201230085, '2025-02-22', 1, 'HADIR'),
+(2, 17, 4337855201230085, '2025-02-22', 1, 'HADIR'),
+(9, 2, 4337855201230085, '2025-02-22', 2, 'TIDAK HADIR'),
+(12, 33, 4337855201230085, '2025-02-22', 1, 'HADIR');
 
 -- --------------------------------------------------------
 
@@ -59,7 +63,9 @@ CREATE TABLE `dosen` (
 --
 
 INSERT INTO `dosen` (`nidn`, `nama_dosen`, `fakultas`) VALUES
-(123456789, 'Adhi Nur Fajar', 'FICT');
+(123456789, 'Adhi Nur Fajar', 'FICT'),
+(123456790, 'Christopan Tangguh', 'FICT'),
+(123456791, 'Safla Alfarisyi', 'FICT');
 
 -- --------------------------------------------------------
 
@@ -72,8 +78,10 @@ CREATE TABLE `jadwal` (
   `id_mk` varchar(255) NOT NULL,
   `nidn` bigint(20) NOT NULL,
   `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu') NOT NULL,
+  `tanggal` date DEFAULT NULL,
   `jam_mulai` time NOT NULL,
   `jam_selesai` time NOT NULL,
+  `pertemuan` int(2) NOT NULL,
   `ruangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -81,8 +89,55 @@ CREATE TABLE `jadwal` (
 -- Dumping data for table `jadwal`
 --
 
-INSERT INTO `jadwal` (`id_jadwal`, `id_mk`, `nidn`, `hari`, `jam_mulai`, `jam_selesai`, `ruangan`) VALUES
-(1, 'INF4001', 123456789, 'Senin', '18:00:00', '18:00:00', 'B2-1');
+INSERT INTO `jadwal` (`id_jadwal`, `id_mk`, `nidn`, `hari`, `tanggal`, `jam_mulai`, `jam_selesai`, `pertemuan`, `ruangan`) VALUES
+(1, 'INFC117', 123456789, 'Senin', '2025-02-03', '18:00:00', '22:10:00', 1, 'B2-1'),
+(2, 'INFC117', 123456789, 'Senin', '2025-02-10', '18:00:00', '22:10:00', 2, 'B2-1'),
+(3, 'INFC117', 123456789, 'Senin', '2025-02-17', '18:00:00', '22:10:00', 3, 'B2-1'),
+(4, 'INFC117', 123456789, 'Senin', '2025-02-24', '18:00:00', '22:10:00', 4, 'B2-1'),
+(5, 'INFC117', 123456789, 'Senin', '2025-03-03', '18:00:00', '22:10:00', 5, 'B2-1'),
+(6, 'INFC117', 123456789, 'Senin', '2025-03-10', '18:00:00', '22:10:00', 6, 'B2-1'),
+(7, 'INFC117', 123456789, 'Senin', '2025-03-17', '18:00:00', '22:10:00', 7, 'B2-1'),
+(8, 'INFC117', 123456789, 'Senin', '2025-03-24', '18:00:00', '22:10:00', 8, 'B2-1'),
+(9, 'INFC117', 123456789, 'Senin', '2025-03-31', '18:00:00', '22:10:00', 9, 'B2-1'),
+(10, 'INFC117', 123456789, 'Senin', '2025-04-07', '18:00:00', '22:10:00', 10, 'B2-1'),
+(11, 'INFC117', 123456789, 'Senin', '2025-04-14', '18:00:00', '22:10:00', 11, 'B2-1'),
+(12, 'INFC117', 123456789, 'Senin', '2025-04-21', '18:00:00', '22:10:00', 12, 'B2-1'),
+(13, 'INFC117', 123456789, 'Senin', '2025-04-28', '18:00:00', '22:10:00', 13, 'B2-1'),
+(14, 'INFC117', 123456789, 'Senin', '2025-05-05', '18:00:00', '22:10:00', 14, 'B2-1'),
+(15, 'INFC117', 123456789, 'Senin', '2025-05-12', '18:00:00', '22:10:00', 15, 'B2-1'),
+(16, 'INFC117', 123456789, 'Senin', '2025-05-19', '18:00:00', '22:10:00', 16, 'B2-1'),
+(17, 'INFC115', 123456790, 'Selasa', '2025-02-04', '18:00:00', '22:10:00', 1, 'LAB-A'),
+(18, 'INFC115', 123456790, 'Selasa', '2025-02-11', '18:00:00', '22:10:00', 2, 'LAB-A'),
+(19, 'INFC115', 123456790, 'Selasa', '2025-02-18', '18:00:00', '22:10:00', 3, 'LAB-A'),
+(20, 'INFC115', 123456790, 'Selasa', '2025-02-25', '18:00:00', '22:10:00', 4, 'LAB-A'),
+(21, 'INFC115', 123456790, 'Selasa', '2025-03-04', '18:00:00', '22:10:00', 5, 'LAB-A'),
+(22, 'INFC115', 123456790, 'Selasa', '2025-03-11', '18:00:00', '22:10:00', 6, 'LAB-A'),
+(23, 'INFC115', 123456790, 'Selasa', '2025-03-18', '18:00:00', '22:10:00', 7, 'LAB-A'),
+(24, 'INFC115', 123456790, 'Selasa', '2025-03-25', '18:00:00', '22:10:00', 8, 'LAB-A'),
+(25, 'INFC115', 123456790, 'Selasa', '2025-04-01', '18:00:00', '22:10:00', 9, 'LAB-A'),
+(26, 'INFC115', 123456790, 'Selasa', '2025-04-08', '18:00:00', '22:10:00', 10, 'LAB-A'),
+(27, 'INFC115', 123456790, 'Selasa', '2025-04-15', '18:00:00', '22:10:00', 11, 'LAB-A'),
+(28, 'INFC115', 123456790, 'Selasa', '2025-04-22', '18:00:00', '22:10:00', 12, 'LAB-A'),
+(29, 'INFC115', 123456790, 'Selasa', '2025-04-29', '18:00:00', '22:10:00', 13, 'LAB-A'),
+(30, 'INFC115', 123456790, 'Selasa', '2025-05-06', '18:00:00', '22:10:00', 14, 'LAB-A'),
+(31, 'INFC115', 123456790, 'Selasa', '2025-05-13', '18:00:00', '22:10:00', 15, 'LAB-A'),
+(32, 'INFC115', 123456790, 'Selasa', '2025-05-20', '18:00:00', '22:10:00', 16, 'LAB-A'),
+(33, 'INFC116', 123456791, 'Rabu', '2025-02-05', '18:00:00', '22:10:00', 1, 'LAB-A'),
+(34, 'INFC116', 123456791, 'Rabu', '2025-02-12', '18:00:00', '22:10:00', 2, 'LAB-A'),
+(35, 'INFC116', 123456791, 'Rabu', '2025-02-19', '18:00:00', '22:10:00', 3, 'LAB-A'),
+(36, 'INFC116', 123456791, 'Rabu', '2025-02-26', '18:00:00', '22:10:00', 4, 'LAB-A'),
+(37, 'INFC116', 123456791, 'Rabu', '2025-03-05', '18:00:00', '22:10:00', 5, 'LAB-A'),
+(38, 'INFC116', 123456791, 'Rabu', '2025-03-12', '18:00:00', '22:10:00', 6, 'LAB-A'),
+(39, 'INFC116', 123456791, 'Rabu', '2025-03-19', '18:00:00', '22:10:00', 7, 'LAB-A'),
+(40, 'INFC116', 123456791, 'Rabu', '2025-03-26', '18:00:00', '22:10:00', 8, 'LAB-A'),
+(41, 'INFC116', 123456791, 'Rabu', '2025-04-02', '18:00:00', '22:10:00', 9, 'LAB-A'),
+(42, 'INFC116', 123456791, 'Rabu', '2025-04-09', '18:00:00', '22:10:00', 10, 'LAB-A'),
+(43, 'INFC116', 123456791, 'Rabu', '2025-04-16', '18:00:00', '22:10:00', 11, 'LAB-A'),
+(44, 'INFC116', 123456791, 'Rabu', '2025-04-23', '18:00:00', '22:10:00', 12, 'LAB-A'),
+(45, 'INFC116', 123456791, 'Rabu', '2025-04-30', '18:00:00', '22:10:00', 13, 'LAB-A'),
+(46, 'INFC116', 123456791, 'Rabu', '2025-05-07', '18:00:00', '22:10:00', 14, 'LAB-A'),
+(47, 'INFC116', 123456791, 'Rabu', '2025-05-14', '18:00:00', '22:10:00', 15, 'LAB-A'),
+(48, 'INFC116', 123456791, 'Rabu', '2025-05-21', '18:00:00', '22:10:00', 16, 'LAB-A');
 
 -- --------------------------------------------------------
 
@@ -101,8 +156,13 @@ CREATE TABLE `mata_kuliah` (
 --
 
 INSERT INTO `mata_kuliah` (`id_mk`, `nama_mk`, `sks`) VALUES
-('INF4001', 'Web Programming 2', 4),
-('INF4002', 'ISAD 2', 4);
+('INFC115', 'Mobile Programming 2', 4),
+('INFC116', 'ISAD 2', 4),
+('INFC117', 'Web Programming 2', 4),
+('INFC119', 'Office Application', 4),
+('INFC120', 'Mobile Apps Project', 4),
+('INFC121', 'OOP Java 2', 4),
+('MKDU305A', 'GWEP 4', 4);
 
 -- --------------------------------------------------------
 
@@ -141,7 +201,7 @@ CREATE TABLE `mhs_mk` (
 --
 
 INSERT INTO `mhs_mk` (`id`, `npm`, `id_mk`) VALUES
-(1, 4337855201230085, 'INF4001');
+(2, 4337855201230085, 'INFC115');
 
 -- --------------------------------------------------------
 
@@ -151,16 +211,19 @@ INSERT INTO `mhs_mk` (`id`, `npm`, `id_mk`) VALUES
 
 CREATE TABLE `user` (
   `id_user` bigint(20) NOT NULL,
-  `password` varchar(8) NOT NULL,
-  `akses` enum('ADMIN','DOSEN','MHS') NOT NULL
+  `password` varchar(16) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
+  `akses` enum('ADMIN','DOSEN','MHS') NOT NULL,
+  `foto_profil` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `password`, `akses`) VALUES
-(4337855201230085, 'WNNDesig', 'MHS');
+INSERT INTO `user` (`id_user`, `password`, `nama_user`, `akses`, `foto_profil`) VALUES
+(4337855201230084, '12345', 'ADHI NUR FAJAR', 'MHS', NULL),
+(4337855201230085, 'WNNDesign21', 'WENDI NUGRAHA NURRAHMANSYAH', 'MHS', 'assets/foto_profil/profil_4337855201230085.png');
 
 -- --------------------------------------------------------
 
@@ -177,11 +240,43 @@ CREATE TABLE `user_view` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `v_d_absensi`
+-- (See below for the actual view)
+--
+CREATE TABLE `v_d_absensi` (
+`id_absensi` int(10) unsigned
+,`id_jadwal` int(10) unsigned
+,`id_mk` varchar(255)
+,`mata_kuliah` varchar(255)
+,`dosen` varchar(255)
+,`hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu')
+,`jam_mulai` time
+,`jam_selesai` time
+,`ruangan` varchar(255)
+,`npm` bigint(20)
+,`mahasiswa` varchar(255)
+,`tanggal` date
+,`pertemuan` int(2)
+,`status` enum('HADIR','TIDAK HADIR','IZIN')
+);
+
+-- --------------------------------------------------------
+
+--
 -- Structure for view `user_view`
 --
 DROP TABLE IF EXISTS `user_view`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `user_view`  AS SELECT `mhs`.`npm` AS `id_user`, `mhs`.`nama` AS `nama_user`, 'Mahasiswa' AS `status` FROM `mhs`union all select `dosen`.`nidn` AS `id_user`,`dosen`.`nama_dosen` AS `nama_user`,'Dosen' AS `status` from `dosen`  ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `v_d_absensi`
+--
+DROP TABLE IF EXISTS `v_d_absensi`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_d_absensi`  AS SELECT `a`.`id_absensi` AS `id_absensi`, `a`.`id_jadwal` AS `id_jadwal`, `j`.`id_mk` AS `id_mk`, `mk`.`nama_mk` AS `mata_kuliah`, `d`.`nama_dosen` AS `dosen`, `j`.`hari` AS `hari`, `j`.`jam_mulai` AS `jam_mulai`, `j`.`jam_selesai` AS `jam_selesai`, `j`.`ruangan` AS `ruangan`, `a`.`npm` AS `npm`, `m`.`nama` AS `mahasiswa`, `a`.`tanggal` AS `tanggal`, `a`.`pertemuan` AS `pertemuan`, `a`.`status` AS `status` FROM ((((`absensi` `a` join `jadwal` `j` on(`a`.`id_jadwal` = `j`.`id_jadwal`)) join `mata_kuliah` `mk` on(`j`.`id_mk` = `mk`.`id_mk`)) join `mhs` `m` on(`a`.`npm` = `m`.`npm`)) join `dosen` `d` on(`j`.`nidn` = `d`.`nidn`)) ;
 
 --
 -- Indexes for dumped tables
@@ -192,7 +287,8 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 ALTER TABLE `absensi`
   ADD PRIMARY KEY (`id_absensi`),
-  ADD KEY `absensi_id_jadwal_index` (`id_jadwal`);
+  ADD KEY `absensi_id_jadwal_index` (`id_jadwal`),
+  ADD KEY `FK_absensi_mhs` (`npm`);
 
 --
 -- Indexes for table `dosen`
@@ -240,19 +336,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id_absensi` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_absensi` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `dosen`
 --
 ALTER TABLE `dosen`
-  MODIFY `nidn` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123456790;
+  MODIFY `nidn` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123456792;
 
 --
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_jadwal` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `mhs`
@@ -264,13 +360,31 @@ ALTER TABLE `mhs`
 -- AUTO_INCREMENT for table `mhs_mk`
 --
 ALTER TABLE `mhs_mk`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4337855201230086;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD CONSTRAINT `FK_absensi_jadwal` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal` (`id_jadwal`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_absensi_mhs` FOREIGN KEY (`npm`) REFERENCES `mhs` (`npm`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `FK_jadwal_dosen` FOREIGN KEY (`nidn`) REFERENCES `dosen` (`nidn`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `FK_jadwal_mata_kuliah` FOREIGN KEY (`id_mk`) REFERENCES `mata_kuliah` (`id_mk`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
